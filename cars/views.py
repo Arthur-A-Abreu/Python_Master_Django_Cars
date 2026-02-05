50f07c8bc6f759eb784725a0e408fd59dbb1c8b2
Arthur Abreu <146493960+Arthur-A-Abreu@users.noreply.github.com>
2026-02-05 18:52:52 -0300

    Refactor cars_view to render car list
    Updated cars_view to fetch all cars from the database and render them in the 'cars.html' template.

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from cars.models import Car
 
 def cars_view(request):
-    return HttpResponse("Welcome to the Cars page!")
+    cars = Car.objects.all()
 
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': cars}
+    )
